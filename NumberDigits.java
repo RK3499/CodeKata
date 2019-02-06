fa5dd7998518446f4697447be28aa9618dde61f7
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-02-06 15:42:45 +0530

    Update NumberDigits.java

--- NumberDigits.java
+++ NumberDigits.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
-class NumberDigits 
+
+public class NumberofDigitsUsingWhile 
 {
-
-    public static void main(String[] args) 
-    {
-
-        int count = 0, num = 3452;
-
-        while(num != 0)
-        {
-            num /= 10;
-            ++count;
-        }
-
-        System.out.println(count);
-    }
+	private static Scanner sc;
+	
+	public static void main(String[] args) 
+	{
+		int Number, Count=0;
+		sc = new Scanner(System.in);
+		Number = sc.nextInt();
+		
+		while(Number > 0) 
+		{
+			Number = Number / 10;
+			Count = Count + 1; 
+		}
+		System.out.println(Count);
+	}
 }
