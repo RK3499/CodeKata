07661db9c8ff68a43a2390ec0f4be7d355aa332f
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-02-06 12:22:45 +0530

    Create VowelOrConsonant.java

---
+++ VowelOrConsonant.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class JavaExample
+{
+   public static void main(String[ ] arg)
+   {
+	boolean isVowel=false;
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Input:");
+	char ch=scanner.next().charAt(0);
+  System.out.print("\n"+ch+"\n");
+  System.out.println("Output:");
+	scanner.close();
+	switch(ch)
+	{
+	   case 'a' :
+	   case 'e' :
+     case 'i' :
+	   case 'o' :
+	   case 'u' :
+	   case 'A' :
+	   case 'E' :
+	   case 'I' :
+	   case 'O' :
+	   case 'U' : isVowel = true;
+	}
+	if(isVowel == true) 
+  {
+	   System.out.println("Vowel");
+	}
+	else 
+  {
+	   if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		  System.out.println("Consonant");
+	   else
+		  System.out.println("invalid");		
+        
+       }
+   }
+}
