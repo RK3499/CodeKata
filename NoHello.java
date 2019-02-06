01c4786ac393b2ddc6b1b80ad6b0a0286abe3cd5
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-02-06 14:38:22 +0530

    Create NoHello.java

---
+++ NoHello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+class NoHello
+{
+  public static void main(String args[])
+  {
+    int n,i;
+    System.out.println("Input");
+    Scanner n = new Scanner(System.in);
+ for(i=1;i<=n;i++)
+ {
+ System.out.println("Hello");
+ }
+  }
+}
