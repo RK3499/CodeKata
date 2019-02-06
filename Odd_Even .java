e94dd86594e858cbc8cd840d37c1f2ea656e24c3
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-02-06 11:43:27 +0530

    Update Odd_Even .java

--- Odd_Even .java
+++ Odd_Even .java
@@ -7,22 +7,22 @@ class Odd_Even
         try
         {
             Scanner s = new Scanner(System.in);
-            System.out.print("Input:"+"\n");
+            System.out.print("Input:");
             int n = s.nextInt();
-            System.out.print(n);
-            System.out.println("\nOutput:\n");
+            System.out.print("\n"+n+"\n");
+            System.out.println("Output:");
             if(n%2==0)
             {
                 
-                System.out.println("\nEven");
+                System.out.println("Even");
             }
-            else if(n%2!=0)
+            else if(n<=0)
             {
-                System.out.println("\nOdd");
+                System.out.println("invalid");
             }
             else
             {
-                System.out.println("\nInvalid");
+                System.out.println("Odd");
             }
         }
         catch(Exception e)
