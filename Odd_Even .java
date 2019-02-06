7904ce7ff35b62b50a90844a3c11f472389dde53
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-02-06 11:33:45 +0530

    Create Odd_Even .java

---
+++ Odd_Even .java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Odd_Even 
+{
+    public static void main(String[] args) throws Exception
+    {
+        try
+        {
+            Scanner s = new Scanner(System.in);
+            System.out.print("Input:\n");
+            int n = s.nextInt();
+            System.out.print(n);
+            System.out.println("\nOutput:\n");
+            if(n%2==0)
+            {
+                
+                System.out.println("Even");
+            }
+            else
+            {
+                System.out.println("Odd");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
