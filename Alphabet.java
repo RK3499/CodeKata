693a3c3774ce3b51b04b8b43d48c347c8e4c3b97
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-02-06 14:15:56 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -1,9 +1,10 @@
+import java.util.*;
+
 public class Alphabet {
 
     public static void main(String[] args) {
 
         Scanner scanner=new Scanner(System.in);
-	      System.out.print("Input:");
 	      char ch=scanner.next().charAt(0);
 
         if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
