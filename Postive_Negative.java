73e089f727d0fad3a2e893b755e14b57d00aac8a
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-02-06 11:21:49 +0530

    Update Postive_Negative.java

--- Postive_Negative.java
+++ Postive_Negative.java
@@ -7,19 +7,22 @@ class Postive_Negative
         try
         {
             Scanner s = new Scanner(System.in);
-            System.out.print("Enter the number you want to check:");
+            System.out.print("INPUT");
             int n = s.nextInt();
             if(n > 0)
             {
+                System.out.println("OUTPUT");
                 System.out.println("Positive");
             }
             else if(n < 0)
             {
+                System.out.println("OUTPUT");
                 System.out.println("Negative");
             }
             else
             {
-                System.out.println("Nuteral");
+                System.out.println("OUTPUT");
+                System.out.println("Zero");
             }
         }
         catch(Exception e)
