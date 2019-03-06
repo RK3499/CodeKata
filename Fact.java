c102ee5202c1e9f4cbbad308cccb4a29bbdd0c39
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-06 21:47:22 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;  
- class Fact {  
-   public static void main(String[] args) {  
+
+ class Fact 
+ {  
+   public static void main(String[] args) 
+   {  
        Scanner s = new Scanner(System.in);  
         
        int number = s.nextInt();  
+       int i,fact=1;  
     
-  int i,fact=1;  
-    
-  for(i=1;i<=number;i++){    
+  for(i=1;i<=number;i++)
+  {    
       fact=fact*i;    
   }    
-  System.out.println(fact);    
+  System.out.println(fact);  
  
-}
+ }
 }  
