d3eb1090160d386258c19a5fa625c3f51ce928ae
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-07 07:44:58 -0800

    Create bg68.java

---
+++ bg68.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ class divby7
+{
+    public static void main(String args[])
+    {
+        
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+    
+       
+            if(m%7==0)
+            {
+                System.out.println("yes");
+            }
+            else
+            {
+                System.out.println("no");
+            }
+    }
+}
+© 2019 GitHub, Inc.
