bbd7988eb41aa2ca3c9c4040ef1d990ee514a20c
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-07 08:14:52 -0800

    Create bg74.java

---
+++ bg74.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class roundint
+{
+   public static void main(String args[])
+   {
+        float num;
+        Scanner in = new Scanner(System.in);
+        num = in.nextFloat();
+         
+          System.out.println(Math.round(Math.ceil(num)));
+        
+   }
+} 
