60cd4e08049aafc8f5d66f8bc359dcde7bc03b80
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-07 11:13:29 +0530

    Create stringrepeat.java

---
+++ stringrepeat.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class stringrepeat{  
+public static void main(String args[])  
+{   
+Scanner input=new Scanner(System.in);
+String str=input.next();
+int count=input.nextInt();         
+ for(int i=0;i<count;i++){System.out.println(str);} 
+}
+} 
