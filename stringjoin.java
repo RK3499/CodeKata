4c08672cb5e8353866fa9983927474723a3209dd
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-08 15:21:06 +0530

    Create stringjoin.java

---
+++ stringjoin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class stringjoin
+{
+public static void main(String ag[])
+{
+Scanner sc=new Scanner(System.in);
+String str1=new String();
+String str2=new String();
+str1=sc.nextLine();
+str2=sc.nextLine();
+
+System.out.println(str1+str2);
+}
+}
