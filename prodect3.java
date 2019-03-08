b208777c431d62a80cc59ea085fe9932ab3783ae
RK3499 <43613787+RK3499@users.noreply.github.com>
2019-03-08 15:02:29 +0530

    Create prodect3.java

---
+++ prodect3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class prodect{
+    public static void main(String args[]){  
+        String str;
+        Scanner s = new Scanner(System.in);
+        str = s.next();
+        int n=s.nextInt();
+        int len=str.length();
+        int find=len-n;
+        System.out.println(str.substring(find,len));
+    }
+}
